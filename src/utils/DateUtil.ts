596e93902af8fafffbd62b21b02fc0984f615123
Sorrawit Kwanja <sorrawit02546@gmail.com>
2025-06-23 15:13:58 +0700

    Implemented formatDateTime util

---
+++ src/utils/DateUtil.ts
@@ -0,0 +1,16 @@
+// dd/mm/yyyy hh:MM:ss
+export const formatDateTime = (dateInput: string) => {
+  const date = new Date(dateInput);
+
+  const pad = (n: number) => n.toString().padStart(2, "0");
+
+  const day = pad(date.getDate());
+  const month = pad(date.getMonth() + 1);
+  const year = date.getFullYear();
+
+  const hours = pad(date.getHours());
+  const minutes = pad(date.getMinutes());
+  const seconds = pad(date.getSeconds());
+
+  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+};
